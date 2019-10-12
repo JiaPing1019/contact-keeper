ae7951afa3f9a869590904b5bfa07429b96ebdc2
JiaPing <jumpping4@gmail.com>
2019-10-12 15:27:55 +0900

    Add Navbar, About and Home in Client

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+ 
+const NavBar = ({icon, title}) => {
+  return (
+    <nav className='navbar bg-primary'>
+      <h1>
+        <i className={icon} />{title}
+      </h1>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
+    </nav>
+  );  
+}
+
+NavBar.defaultProps = { 
+  title: 'Content Keeper',
+  icon: 'fas fa-id-card-alt'
+}
+
+NavBar.propTypes = { 
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired
+}
+
+export default NavBar;
---
+++ client/src/components/pages/About.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react';
+                               
+const About = () => {          
+  return (
+    <Fragment>                 
+      <h1>About This App</h1>  
+      <p className='my-1'>
+        This is full stack React app for keeping contact
+        App to search Github users
+      </p>
+      <p className='bg-dark p'>
+        <strong>Version: </strong>1.0.0
+      </p>     
+    </Fragment>                
+  );
+}; 
+    
+export default About;
