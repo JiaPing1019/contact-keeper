4f8214249f9107f9ab7c40c341a96d97fbaacc6d
JiaPing <jumpping4@gmail.com>
2019-11-04 16:20:44 +0900

    Add Context, Reducer and State to Auth

---
+++ client/src/context/auth/authReducer.js
@@ -0,0 +1,6 @@
+import {                       
+  // Add later
+} from '../types';  
+
+export default (state, action) => {
+}
---
+++ client/src/context/auth/authState.js
@@ -0,0 +1,45 @@
+import React, { useReducer } from 'react';
+import uuid from 'uuid';     
+import AuthContext from './authContext';
+import AuthReducer from './authReducer';
+import {                       
+  // Add later...
+} from '../types';  
+
+const AuthState = props => {
+  const initialState = {
+    token: localStorage.getItem('token'),
+    isauthenticated: null,
+    loading: true,
+    user: null,
+    error: null
+  }
+
+  const [state, dispatch] = useReducer(AuthReducer, initialState);
+
+  // Load User
+
+  // Register User
+
+  // Login User
+
+  // Logout
+
+  // Clear Errors
+
+  return (
+    <AuthContext.Provider
+      value={{
+       token: state.token,
+       isauthenticated: state.isauthenticated,
+       loading: state.loading,
+       user: state.user,
+       error: state.error
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  ); 
+}
+
+export default AuthState;
