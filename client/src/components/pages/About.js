b946461e3326b7e1186867ba32451bdff2f4db9f
JiaPing <jumpping4@gmail.com>
2019-12-15 14:41:26 +0900

    Remove package-lock.json

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -1,18 +1,17 @@
-import React, { Fragment } from 'react';
-                               
-const About = () => {          
+import React, {Fragment} from 'react';
+
+const About = () => {
   return (
-    <Fragment>                 
-      <h1>About This App</h1>  
-      <p className='my-1'>
-        This is full stack React app for keeping contact
-        App to search Github users
+    <Fragment>
+      <h1>About This App</h1>
+      <p className="my-1">
+        This is full stack React app for keeping contact App to search Github users
       </p>
-      <p className='bg-dark p'>
+      <p className="bg-dark p">
         <strong>Version: </strong>1.0.0
-      </p>     
-    </Fragment>                
+      </p>
+    </Fragment>
   );
-}; 
-    
+};
+
 export default About;
